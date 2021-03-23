73659fa0b346b85daaf0ac0546148f6ebae7b272
nigoh <hironobunigo@gmail.com>
2021-03-23 23:15:03 +0900

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import liff from '@line/liff/dist/lib';
-
+/*
 async function sendMessage() {
   try {
     await liff.init({ liffId : process.env.REACT_APP_LIFF_ID as string });
@@ -35,6 +35,45 @@ async function getUserInfo() {
     window.alert('Error sending message: ' + error);
   }
 }
+*/
+  /* 追加: メッセージ送信 */
+  const sendMessage = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string}) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) { // LIFFので動いているのであれば
+          liff.sendMessages([{ // メッセージを送信する
+            'type': 'text',
+            'text': "You've successfully sent a message! Hooray!"
+          }]).then(function() {
+            window.alert('Message sent');
+          }).catch(function(error) {
+            window.alert('Error sending message: ' + error);
+          });
+        }
+      })
+  }
+
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string})
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) {
+          liff.getProfile()  // ユーザ情報を取得する
+            .then(profile => {
+              const userId: string = profile.userId
+              const displayName: string = profile.displayName
+              alert(`Name: ${displayName}, userId: ${userId}`)
+            }).catch(function(error) {
+              window.alert('Error sending message: ' + error);
+            });
+        }
+      })
+
+  }
 
 function App() {
   return (
