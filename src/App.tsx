d72c5f99592bdac7ea14284ba101364307ba5926
nigoh <hironobunigo@gmail.com>
2021-03-23 22:09:03 +0900

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import liff from '@line/liff/dist/lib';
+
+async function sendMessage() {
+  try {
+    await liff.init({ liffId : process.env.REACT_APP_LIFF_ID as string });
+    if(!liff.isLoggedIn()) {
+      liff.login({});
+    } else if (liff.isInClient()) {
+      await liff.sendMessages([{
+        'type': 'text',
+        'text': "You've successfully sent a message! Hooray!"
+      }]);
+      window.alert('Message sent');
+    }
+  } catch(error) {
+    window.alert('Error sending message: ' + error);
+  }
+}
+
+async function getUserInfo() {
+  try {
+    await liff.init({ liffId : process.env.REACT_APP_LIFF_ID as string });
+    if(!liff.isLoggedIn()) {
+      liff.login({});
+    } else if (liff.isInClient()) {
+      const profile : any = await liff.getProfile();
+      const userId : string = profile.userId;
+      const displayName: string = profile.displayName;
+      alert(`Name: ${displayName}, userId: ${userId}`);
+    }
+  } catch (error) {
+    window.alert('Error sending message: ' + error);
+  }
+}
 
 function App() {
   return (
